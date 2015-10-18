5f2224f7796eb6220d29eddd2cbba87363e99834
assslove <houbin-12@163.com>
2015-10-18 21:23:30 +0800

    generator proto desc

--- GameServer/proto/gen.bat
+++ GameServer/proto/gen.bat
@@ -1,1 +1,2 @@
- protoc --descriptor_set_out=desc/gm.desc --include_imports gm.proto
+protoc --descriptor_set_out=desc/code.desc --include_imports code.proto
+protoc --descriptor_set_out=desc/user.desc --include_imports user.proto
