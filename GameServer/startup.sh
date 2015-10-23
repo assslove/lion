51a9e05bc769ea0bb442d6e14c089e0ad8476aa7
houbin <houbin-12@163.com>
2015-10-23 17:30:38 +0800

    add some

--- GameServer/startup.sh
+++ GameServer/startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 #NODE_ENV 环境 development | production
-#第3个参数端口号
-#NODE_ENV=production node app.js -p 8001 & >/dev/null 2>1&
-#NODE_ENV=development node app.js -p 8001 & >/dev/null 2>1&
 #auto reboot forever
-NODE_ENV=production forever app.js -p 8001 > /dev/null 2>&1 &
+NODE_ENV=production forever app.js -p 8000 > /dev/null 2>&1 &
