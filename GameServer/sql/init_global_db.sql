dc8d6bbd18e8e974a6b52bd5b1b889d94928c889
houbin <houbin-12@163.com>
2015-10-30 10:03:34 +0800

    fix sql

--- GameServer/sql/init_global_db.sql
+++ GameServer/sql/init_global_db.sql
@@ -5,6 +5,7 @@ drop table if exists t_uid;
 create table t_uid (
 	`uid` int unsigned NOT NULL COMMENT '用户id',
 	`flag` tinyint unsigned NOT NULL COMMENT '标志 0-未用 1-已用',
+	primary key(`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #初始化账号表 与其它账号绑定关系
@@ -13,4 +14,6 @@ create table t_account (
 	`uid` int unsigned NOT NULL COMMENT '用户id',
 	`qq` int unsigned NOT NULL default 0 COMMENT 'qq号',
 	`wechat` varchar(32) default  '0' COMMENT '微信号'
+	primary key(`uid`),
+	index(`qq`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
