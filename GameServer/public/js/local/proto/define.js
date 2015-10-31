9cd8a787cc0039cd4ef2ca5b371ef5a450335756
unknown <bin.hou@cnsha-01477-win.dena.local>
2015-10-31 17:07:14 +0800

    add some

--- GameServer/public/js/local/proto/define.js
+++ GameServer/public/js/local/proto/define.js
@@ -1,12 +1,13 @@
 //normal define
 var proto = {
     USER_CREATE : 1,    //创建用户
-    USER_LOGIN : 2,     //登录
+    USER_LOGIN : 2,     //除用户创建外，所有请求必须先登录
     USER_LOGOUT : 3,    //登出
-    USER_ERROR : 4,     //错误返回
-    USER_SYNC_INFO : 5, //同步玩家数据
-    USER_SYNC_ITEM : 6, //同步物品
+    USER_SYNC_INFO : 4, //同步玩家数据
+    USER_SYNC_ITEM : 5, //同步物品
+    USER_SYNC_TIME_ITEM : 6, //同步限时物品
     USER_SYNC_COPY : 7, //同步关卡
+    USER_GET_INFO : 8   //同步服务器数据 包括玩家数据,物品，限时物品,关卡等
 };
 
 //错误码 编号与描述，方便客户端显示
@@ -16,18 +17,18 @@ var error_code =  {
     PROTO_LEN_INVALID:  [3,  "协议长度不正确"],
     PROTO_DATA_INVALID:  [4,  "协议数据不正确"],
     USER_SESSION_EXPIRE:  [5,  "用户session过期"],
-    MSG_NOT_FOUND:  [6,  "返回消息体没有找到"],
+    MSG_NOT_FOUND:  [6,  "返回消息体没有找到"]
 };
 
-var DEFINE = {
+var ret = {
     PROTO : proto,
     ERROR_CODE : error_code
 };
 
 var head = {
-    p: '协议id',
-    s: '序列号或者返回码'
+    p : '协议id',
+    s : '序列号或者返回码'
 };
 
-//module.exports = DEFINE;
+module.exports = ret;
 
