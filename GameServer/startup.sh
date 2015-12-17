717706770e70048fb292b11a7cc90c19eb970bb4
houbin <houbin-12@163.com>
2015-12-17 09:17:40 +0800

    fix start script

--- GameServer/startup.sh
+++ GameServer/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 pkill -9 node
 sleep 1
-NODE_ENV=production forever app.js -p 8001 > /dev/null 2>&1 &
+for i in `seq 8001 8003`
+do
+	mkdir -p log/$i
+	NODE_ENV=production forever app.js -p $i > /dev/null 2>&1 &
+done
 
